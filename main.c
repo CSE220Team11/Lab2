0b7d8461593c3e09f2b43bc15e1aab214178a44f
amberlcole <amber.l.cole@asu.edu>
2014-02-23 00:38:07 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,13 @@ int main (int argc, const char *argv[])
     char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     
-   /* Missing Code Here */
+    /* Amber code starts here*/
+    init_lister(const char *argv[], char source_name[], char date[] );
+    while( get_source_line( FILE *file, char source_name[], char date[] ) == TRUE )
+    {
+    	get_source_line( FILE *file, char source_name[], char date[] );
+    	//Not suure if FILE *file is correct yet, not on a computer that I can test on currently
+    }
     return 0;
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
@@ -23,9 +29,14 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
     static int line_number = 0;
     
-    if (1) //This is missing a condition
+    if (fgets( char *source_buffer[], int MAX_SOURCE_LINE_LENGTH, FILE *src_file) != NULL ) 
     {
-		/*  Missing Code Here */
+    	//Amber code starts here
+	fgets( char *source_buffer[], int MAX_SOURCE_LINE_LENGTH, FILE *src_file);
+    	line_number++; 
+    	sprintf( char *print_buffer, char *source_buffer[], int line_number );
+    	//ends here
+    	
         return (TRUE);
     }
     else
